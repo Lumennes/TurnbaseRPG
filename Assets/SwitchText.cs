c6de6c9a644fd19d42ae8fa9db0263a9cc118b22
Lumennes <lumennes@gmail.com>
2022-07-01 00:30:42 +0500

    Initial commit

---
+++ Assets/SwitchText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SwitchText : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(5f);
+
+        text.text = $"Прошло времени { Time.realtimeSinceStartup }";
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
